1088b13613178bba3fae640f8e06b8283c09de16
Anil A <leoanil2002@gmail.com>
2024-05-31 12:10:49 +0530

    Create Button.js

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+// src/Button.js
+import React from 'react';
+import './Button.css';
+
+const Button = () => {
+  return (
+    <button className="feedback-button">
+      Give Feedback
+    </button>
+  );
+};
+
+export default Button;
